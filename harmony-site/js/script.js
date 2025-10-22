82205c2951e7b78356c58915fb8426c0ff88c5b0
abeher00 <abeher00@g.uafs.edu>
2025-10-22 03:24:39 -0500

    added new site

---
+++ harmony-site/js/script.js
@@ -0,0 +1,34 @@
+
+
+const div1 = document.getElementById('spinningParagraph1');
+const words1 = div1.textContent.split(' ');
+div1.innerHTML = ''; // Clear the original content
+
+words1.forEach(word => {
+  const span = document.createElement('span');
+  span.textContent = word + ' ';
+  span.classList.add('spinning-word1');
+  div1.appendChild(span);
+});
+
+const div2 = document.getElementById('spinningParagraph2');
+const words2 = div2.textContent.split(' ');
+div2.innerHTML = ''; // Clear the original content
+
+words2.forEach(word => {
+  const span = document.createElement('span');
+  span.textContent = word + ' ';
+  span.classList.add('spinning-word2');
+  div2.appendChild(span);
+});
+
+const div3 = document.getElementById('spinningParagraph3');
+const words3 = div3.textContent.split(' ');
+div3.innerHTML = ''; // Clear the original content
+
+words3.forEach(word => {
+  const span = document.createElement('span');
+  span.textContent = word + ' ';
+  span.classList.add('spinning-word3');
+  div3.appendChild(span);
+});
