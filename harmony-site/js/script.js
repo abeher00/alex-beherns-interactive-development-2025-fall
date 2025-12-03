af6b88268a947d349eedfdbe1696f658d16d9ab6
abeher00 <abeher00@g.uafs.edu>
2025-12-03 04:02:00 -0600

    added new pages

---
+++ harmony-site/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    $('#disk.border-container').hover(
+        function() {
+            $(this).addClass('hover-active');
+        },
+        function() {
+            $(this).removeClass('hover-active');
+        }
+    );
+ });
+
+
+ $(document).ready(function() {
+    $('#dual.border-container').hover(
+        function() {
+            $(this).addClass('hover-active');
+        },
+        function() {
+            $(this).removeClass('hover-active');
+        }
+    );
+ });
+
+  $(document).ready(function() {
+    $('#play.border-container').hover(
+        function() {
+            $(this).addClass('hover-active');
+        },
+        function() {
+            $(this).removeClass('hover-active');
+        }
+    );
+ });
+
+   $(document).ready(function() {
+    $('#tube.border-container').hover(
+        function() {
+            $(this).addClass('hover-active');
+        },
+        function() {
+            $(this).removeClass('hover-active');
+        }
+    );
+ });
